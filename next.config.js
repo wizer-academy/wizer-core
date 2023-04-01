d5ce2d7b47c8a5bd081a2ec02daa599f00fad962
Lucas Alves de Araujo <lucas.al.araujo@hotmail.com>
2023-04-01 12:03:27 -0300

    chore: init project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
+<<<<<<< HEAD
 const nextConfig = {}
+=======
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+}
+>>>>>>> c9e1b52 (chore: init project)
 
 module.exports = nextConfig
---
+++ src/app/(pages)/login/components/button/index.tsx
@@ -0,0 +1,14 @@
+'use client'
+import Styles from 'styled-components'
+
+const LoginBtt = Styles.button`
+    background: #ef2cbf
+`
+
+export default function Button() {
+    return (
+        <>
+        <LoginBtt>Botão</LoginBtt>
+        </>
+    )
+  }
