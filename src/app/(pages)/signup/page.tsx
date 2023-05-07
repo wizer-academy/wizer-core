e224158aa3cda849f267d762d6054422a36f0761
pitossomo <pitossomo@gmail.com>
2023-05-07 11:29:01 -0300

    feat(signup): integration with backend

--- src/app/(pages)/signup/page.tsx
+++ src/app/(pages)/signup/page.tsx
@@ -1,19 +1,20 @@
-'use client'
+"use client";
 import Button from "../../components/button/";
 import Input from "../../components/input/";
 import SignAlternative from "./components/signAlternative/";
 import SwitchForm from "./components/switchForm/";
-import theme from '../../../themes';
-import Styles from 'styled-components'
-import Image from 'next/image'
-import { Poppins } from 'next/font/google'
+import theme from "../../../themes";
+import Styles from "styled-components";
+import Image from "next/image";
+import { Poppins } from "next/font/google";
 import Link from "next/link";
 import { useState } from "react";
+import axios from "axios";
 
 const poppins = Poppins({
-  weight: '400',
-  subsets: ['latin'],
-})
+  weight: "400",
+  subsets: ["latin"],
+});
 
 const ConteinerForm = Styles.form`
   background: ${theme.BACKGROUND};
@@ -31,7 +32,7 @@ const ConteinerForm = Styles.form`
   @media (min-width: 900px) {
     width: 50%;
   }
-`
+`;
 const ConteinerMessage = Styles.div`
   width: 0%;
   height: 100%;
@@ -46,7 +47,7 @@ const ConteinerMessage = Styles.div`
   &::-webkit-scrollbar {
     display: none;
   }
-`
+`;
 const BackImage = Styles.div`
   background-image: url(/images/Rectangle_com_imagem.jpg);
   opacity: 0.2;
@@ -57,7 +58,7 @@ const BackImage = Styles.div`
 
   display: grid;
   place-items: center;
-`
+`;
 const Message = Styles.p`
   position: absolute;
   margin-left: -20vw;
@@ -69,7 +70,7 @@ const Message = Styles.p`
   @media (max-width: 900px) {
     display: none;
   }
-`
+`;
 
 const Conteiner = Styles.div`
   display: flex;
@@ -89,26 +90,41 @@ const Conteiner = Styles.div`
   &::-webkit-scrollbar {
     display: none;
   }
-`
+`;
 
 const ForgetPasssowrd = Styles.p`
   width: 350px;
   text-align: right;
-`
+`;
 
 export default function SignUp() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmedPassword, setConfirmedPassword] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmedPassword, setConfirmedPassword] = useState("");
   const [isCheckedTerms, setIsChecked] = useState(false);
 
-  const handleSubmit = () => {
-    console.log('Name:', name);
-    console.log('Email:', email);
-    console.log('Password:', password);
-    console.log('ConfirmedPassword:', confirmedPassword);
-    console.log('Terms:', isCheckedTerms);
+  const handleSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+
+    console.log("Name:", name);
+    console.log("Email:", email);
+    console.log("Password:", password);
+    console.log("ConfirmedPassword:", confirmedPassword);
+    console.log("Terms:", isCheckedTerms);
+
+    axios
+      .post("http://localhost:4000/user", {
+        email: email,
+        password: password,
+        name: name,
+      })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   const handleOnChange = () => {
@@ -118,10 +134,11 @@ export default function SignUp() {
   return (
     <>
       <Conteiner>
-
         <ConteinerMessage>
           <BackImage />
-          <Message>Você está a um passo do seu <b>futuro</b></Message>
+          <Message>
+            Você está a um passo do seu <b>futuro</b>
+          </Message>
         </ConteinerMessage>
 
         <ConteinerForm onSubmit={handleSubmit}>
@@ -133,29 +150,37 @@ export default function SignUp() {
             value={name}
             onChange={(e: any) => setName(e.target.value)}
             placeholder="Digite aqui seu nome"
-            type='name'
-          >Nome completo</Input>
+            type="name"
+          >
+            Nome completo
+          </Input>
           <Input
             value={email}
             onChange={(e: any) => setEmail(e.target.value)}
             placeholder="Digite aqui seu e-mail"
-            type='email'
+            type="email"
             hasHiddenButton={false}
-          >E-mail</Input>
+          >
+            E-mail
+          </Input>
           <Input
             value={password}
             onChange={(e: any) => setPassword(e.target.value)}
             placeholder="Digite aqui sua senha"
-            type='password'
+            type="password"
             hasHiddenButton={true}
-          >Senha</Input>
+          >
+            Senha
+          </Input>
           <Input
             value={confirmedPassword}
             onChange={(e: any) => setConfirmedPassword(e.target.value)}
             placeholder="Digite novamente sua senha"
-            type='password'
+            type="password"
             hasHiddenButton={true}
-          >Confirme sua senha</Input>
+          >
+            Confirme sua senha
+          </Input>
 
           <div className="checboxterms">
             <input
@@ -172,9 +197,7 @@ export default function SignUp() {
           <Button type="submit">Criar</Button>
           <SignAlternative />
         </ConteinerForm>
-
       </Conteiner>
-
     </>
-  )
+  );
 }
